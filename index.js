c22c8b05b411b215468d198eb0b0a6ac4e5a82a9
Stimarc <rikudo7474@gmail.net>
2023-04-11 22:24:05 +0300

    update cycles,methods,array methods

--- index.js
+++ index.js
@@ -1,7 +1,113 @@
-const carArray = ['Bmw','Audi','Mitsubishi','Mersedec','Volvo']
+// Math:
 
-function getcarArray (array){
-    return `${array[0]}, ${array[array.length-1]}`
-}
+//console.log(Math.round(5.7)) //6
+//console.log(Math.random(5.2)) //5
+
+//console.log(Math.floor(5.7)) //5
+//console.log(Math.ceil(5.2)) //6
+
+//console.log(Math.pow(3,4))//81
+//console.log(Math.max(-2,5,3,6,4,3,90,31,43))//90
+//console.log(Math.min(-2,5,3,6,4,3,90,31,43))//-2
+
+//console.log(Math.random())//0 - 1
+
+//const random = (min , max) => Math.floor(Math.random) * (max - min)+ min//random
+
+
+
+
+
+//Цикли:
+
+//for , while ,do while
+
+// const arr = [1,5,6,7,8,4,3,];
+//  let i = 0;
+
+//  while (i< arr.length){ //цикл с передумовою
+//     console.log(arr[i])
+//     i++
+//  }
+
+// do{
+//     console.log(arr[i])
+//         i++
+// } while (i < arr,length)
+
+
+// for(;;){
+//   console.log(arr[1])
+//}
+
+// const people = [
+//     {name: "Dmytro",age:23},
+//     {name: "Liza",age:30},
+//     {name: "Helen",age:36},
+//     {name: "Mykola",age:18},
+//     {name: "Alex",age:58},
+// ]
+
+
+
+
+// for(let i=0; i < people.length; i++){
+//     if(people[i].name !== Mykola)
+// }
     
-console.log(getcarArray(carArray))
+
+
+
+
+// for(let user of people){
+//     console.log(user.name, user.age)
+// }
+
+
+// const name = prompt("Input name")
+// console.log(name)
+
+
+// const myName = prompt ("Введіть імя")
+
+// const getName = (name) =>{
+//     return name
+// }
+
+// document.write(getName(myName))
+
+// const arr = [1, 4, 234, 23, 34, 324]; Виведення сумму чисел в дужках за допомогою id в html 
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// document.getElementById("numbers").textContent = sum;
+
+//Методи массивів:
+
+
+// names.push("Alex")//+ alex
+//names.pop(); - Any
+// names.shift() - Max
+// names.unshift("Alex") + alex
+
+// names.splice(0,3) с нуля видаляе 3 елементи і залишае Any
+// names.slice(0,3)
+
+// const string = names.join("..")
+
+
+// const names = ["Max","Jackson","Vito","Any","Alex"]
+
+
+// const filter = (arr) => {
+//     for(let i = 0; i < arr.length; i++)
+//         if(arr[i].length >3){
+//         console.log("-",arr[i])
+//     }
+// }
+
+// filter(names)
+
+
+
