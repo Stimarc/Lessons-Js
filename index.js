9ff6a02eb0686344501eff07bb888c6b3d66834b
Stimarc <rikudo7474@gmail.net>
2023-04-13 22:21:14 +0300

    update 22:10

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const carArray = ['Bmw','Audi','Mitsubishi','Mersedec','Volvo']
+//Приймає масив чисел і повертає відсортований масив тільки з елементами що більше ніж середнє значення масива.
 
-function getcarArray (array){
-    return `${array[0]}, ${array[array.length-1]}`
-}
-    
-console.log(getcarArray(carArray))
+// function getSortedArrayAboveAvg(arr) {
+//     const avg = arr.reduce((sum, num) => sum + num, 0) / arr.length;
+//     const filteredArr = arr.filter(num => num > avg);
+//     filteredArr.sort((a, b) => a - b);
+//     return filteredArr;
+//   }
+
+//   const sortedArr = getSortedArrayAboveAvg([3, 2, 5, 6, 1]);
+
+// function isPalindrome(str) {
+//     str = str.replace(/[\W_]/g, '');
+//     for (let i = 0; i < str.length / 2; i++) {
+//       const leftChar = str[i];
+//       const rightChar = str[str.length - 1 - i];
+//       if (leftChar !== rightChar && leftChar.toLowerCase() !== rightChar.toLowerCase()) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+  
+//   console.log(isPalindrome("Привіт Дмитро"));
+
+// function doubleNumber(num, count = 0) {
+//     if (num <= 0) {
+//       return [2 ** count, count];
+//     } else {
+//       return doubleNumber(num >> 1, count + 1);
+//     }
+//   }
+  
+//   const [result, count] = doubleNumber(7);
+//   console.log(result);
+//   console.log(count);
+  
