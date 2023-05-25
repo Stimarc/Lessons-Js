9fa5e47a80ed64709322bfb1a889d8a51193addb
Stimarc <rikudo7474@gmail.net>
2023-05-25 22:11:35 +0300

    finish slider

--- Slider1/assets/js/main.js
+++ Slider1/assets/js/main.js
@@ -1,7 +1,23 @@
 $(document).ready(()=>{
 
-   //1
+   // 1
    console.log('Slider1 -> OK' );
-   const slider = new Slider('slider','1000px','500px',[]);
+
+   // 2
+   const manager = new Manager();
+   manager.fillCollection('collection1','p', 10);
+
+   // 3
+   let targetCollection = manager._collection;
+   console.log(`targetCollection: ${targetCollection}`);
+
+   // 4
+   const slider = new Slider('slider','800px','450px',targetCollection,500);
+   slider.loadCollection(0, targetCollection);
+
+   //5 Activator
+   slider.activateArrowHover();
+   slider.activateRightArrow();
+   slider.activateLeftArrow();
 
 });
